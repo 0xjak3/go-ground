83caa36e1d09b27278bd381274daa776413bcb2e
Jak3xz <jak3xz@gmail.com>
2026-02-04 21:22:16 +0700

    feat: install atlas

---
+++ xdb/loader/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"xdb/models"
+
+	"ariga.io/atlas-provider-gorm/gormschema"
+)
+
+func main() {
+	stmts, err := gormschema.New("postgres").Load(&models.User{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load gorm schema: %v\n", err)
+		os.Exit(1)
+	}
+	io.WriteString(os.Stdout, stmts)
+}
--- xdb/main.go
+++ xdb/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"xdb/models"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/joho/godotenv"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB() *gorm.DB {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
+}
+
+func AutoMigrate(db *gorm.DB) {
+	db.AutoMigrate(&models.User{})
+}
+
+// func main() {
+// 	AutoMigrate(InitDB())
+// }
 
 func main() {
-	fmt.Println("Hello World")
+	m, err := migrate.New(
+		"file://migrations",
+		"postgres://admin:admin@localhost:5432/xdb?sslmode=disable",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// รัน Migration ไปยังเวอร์ชันล่าสุด
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal(err)
+	}
 }
